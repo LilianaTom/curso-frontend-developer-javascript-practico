e628002d98ec876cffc6476da3cf887909a9d112
Karen guancha <karenliliguancha@unicauca.edo.co>
2024-05-09 10:22:52 -0500

    Se unfica y añaden estilos

---
+++ script.js
@@ -0,0 +1,95 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuIcon = document.querySelector('.menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+menuIcon.addEventListener('click',toggleMobileMenu);
+shoppingCart.addEventListener('click',toggleCartAside);
+
+function toggleDesktopMenu(){
+    const isAsideMenuClosed = aside.classList.contains('inactive');   
+    
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    const isAsideMenuClosed = aside.classList.contains('inactive');   
+    
+    if(!isAsideMenuClosed){
+        aside.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');   
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');    
+}
+
+const productList=[];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Pantalla',
+    price: 320,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Computador',
+    price: 420,
+    imagen: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+function renderProduct(arr){
+    for(const product of productList){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const  productImg = document.createElement('img');
+        productImg.setAttribute('src',product.imagen);
+    
+        const productInfo= document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+        
+        const productPrice = document.createElement('p');
+        productPrice.innerText ='$'+product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+renderProduct(productList);
